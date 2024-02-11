209e25c40d1fc235c4504c5402b836f2589d32a1
PeterW15534 <peterw15534@gmail.com>
2024-02-11 16:41:06 -0700

    Added the folder

---
+++ lab-2/lab-2/js/index.js
@@ -0,0 +1,13 @@
+function updateInnerHTML (selector, string){
+    document.querySelector(selector).innerHTML = string
+    }
+
+    updateInnerHTML('h1', "New Topic")
+
+    function strong(string){
+        return `<strong>${string}</strong>`
+        }
+        const template = strong('making things useful')
+        console.log(template)
+
+            
